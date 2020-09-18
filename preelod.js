aa6c7d6849e1b2d1539498da08dcb16f1a88f963
Mohammed Faruk <70846759+mh-faruk@users.noreply.github.com>
2020-09-18 14:25:46 +0600

    Add files via upload

---
+++ preelod.js
@@ -0,0 +1,7 @@
+function LoadComplete(){
+    // setTimeout('clear()', 3000);
+    document.querySelector('.preeload_div').style.display='none';
+};
+// function clear(){
+//     document.querySelector('.preeload_div').style.display='none';
+// }
